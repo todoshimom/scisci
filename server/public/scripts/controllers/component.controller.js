1130c2ea49d68296e400664b1363d06f983f3727
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-03-05 16:40:20 -0600

    added get route to component service, components are now being displayed on the dom

--- server/public/scripts/controllers/component.controller.js
+++ server/public/scripts/controllers/component.controller.js
@@ -2,15 +2,18 @@ myApp.controller('ComponentController', ['ComponentService', function (Component
     console.log('ComponentController created');
     let self = this;
 
+    self.componentLibrary = ComponentService.componentLibrary;
+
     self.componentItem = {
       consumable: false,
       general_stock_item: false
     };
 
+    // begin addComponentToLib()
     self.addComponentToLib = function(component) {
       console.log(component);
       ComponentService.addComponentToLib(component);
-    };
+    }; // end addComponentToLib()
 
 
 }]);
--- server/public/scripts/services/component.service.js
+++ server/public/scripts/services/component.service.js
@@ -2,12 +2,26 @@ myApp.service('ComponentService', ['$http', '$location', function ($http, $locat
     console.log('ComponentService Loaded');
     let self = this;
 
+    self.componentLibrary = {list:[]};
+
 
     /******************************************/
     /*              GET REQUESTS              */
     /******************************************/
 
+    // begin getComponents()
+    self.getComponents = function() {
+      $http.get('/api/component')
+        .then( function(response) {
+          console.log(response.data);
+          self.componentLibrary.list = response.data;
+        })
+        .catch( function(error) {
+          console.log(error);
+        });
+    }; // end getComponents()
 
+    self.getComponents();
 
     /******************************************/
     /*             POST REQUESTS              */
@@ -19,6 +33,7 @@ myApp.service('ComponentService', ['$http', '$location', function ($http, $locat
       $http.post('/api/component', component)
         .then( function(response) {
           console.log(`Component added to library:`, response);
+          self.getComponents();
         })
         .catch( function(error) {
           console.log(`Error on POST:`, error);
