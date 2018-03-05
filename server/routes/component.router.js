8004c66c0c61c9617a5cbd815259a1ab6f12dc57
Philip-Owen <31570473+Philip-Owen@users.noreply.github.com>
2018-03-05 16:00:25 -0600

    added post route to components router, sucessfully adding components to db

--- server/routes/component.router.js
+++ server/routes/component.router.js
@@ -14,6 +14,36 @@ const router = express.Router();
 /*             POST REQUESTS              */
 /******************************************/
 
+router.post('/', (req, res) => {
+
+  if(!req.body.hasOwnProperty('vendor_name_secondary')) {
+    req.body.vendor_name_secondary = null;
+  }
+  if(!req.body.hasOwnProperty('vendor_url_secondary')) {
+    req.body.vendor_url_secondary = null;
+  }
+
+  let item = req.body;
+
+  let queryText = `
+  INSERT INTO components ("name", "description", "vendor_name_primary",
+  "vendor_url_primary", "vendor_name_secondary", "vendor_url_secondary",
+  "notes", "price_per_unit", "pieces_per_unit", "consumable", "type",
+  "general_stock_item")
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12); `;
+
+  pool.query(queryText, [item.name, item.description, item.vendor_name_primary, item.vendor_url_primary,
+  item.vendor_name_secondary, item.vendor_url_secondary, item.notes, item.price_per_unit, item.pieces_per_unit,
+  item.consumable, item.type, item.general_stock_item])
+      .then((results) => {
+        console.log('Component insert', results);
+        res.sendStatus(201);
+      })
+      .catch((error) => {
+        console.log('Error making insert', error);
+        res.sendStatus(500);
+      });
+});
 
 
 /******************************************/
