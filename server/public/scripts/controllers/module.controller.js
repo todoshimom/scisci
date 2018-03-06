6cd1c4ac07d44524abe64d584ec1350d773a259c
Andrew Johnson <ajj@byu.net>
2018-03-06 14:28:13 -0600

    Added dummy data

--- server/public/scripts/controllers/module.controller.js
+++ server/public/scripts/controllers/module.controller.js
@@ -2,5 +2,31 @@ myApp.controller('ModuleController', ['ModuleService', function (ModuleService) 
     console.log('ModuleController created');
     let self = this;
 
+    // DUMMY DATA
+    self.data = {};
+    self.data.name = '';
+    self.data.code = '';
+    self.data.estimated_assembly_time = '';
+    self.data.version_number = '';
+    self.data.version_notes = 'asdf';
+    self.data.version_date = '';
+    self.data.module_drive_link = 'asdf';
+    self.data.to_be_printed_link = '';
+    self.data.assembly_video_link = '';
+    self.data.activity_video_link = '';
+    self.data.kit_content_link = '';
+    self.data.other1_title = '';
+    self.data.other1_link = '';
+    self.data.other2_title = '';
+    self.data.other2_link = '';
+    self.data.assembly_notes = '';
+
+    // CALCULATIONS
+    self.calculations = {};
+    self.calculations.material_cost = 0;
+    self.calculations.material_in_kit_cost = 0;
+    self.calculations.estimated_labor_cost = 0;
+    self.calculations.materials_ordered_labor = 0;
+    self.calculations.materials_in_kit_labor = 0;
     
 }]);
