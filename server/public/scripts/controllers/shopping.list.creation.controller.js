f1ab88ec619304704ae74b3e4553c04a037a1e4a
Alaethia <truthcharis@gmail.com>
2018-03-08 11:07:48 -0600

    getting id back

--- server/public/scripts/controllers/shopping.list.creation.controller.js
+++ server/public/scripts/controllers/shopping.list.creation.controller.js
@@ -5,7 +5,7 @@ myApp.controller('ShoppingListCreationController', ['ShoppingListService', funct
     self.createShoppingList = function(name) {
         ShoppingListService.createShoppingList(name);
     };//end function for start list button to database/service 
-
+    
 //get request to get modules for search with filter that adds them to the first table automatically after an update
     self.getModules = function(keyword) {
         ShoppingListService.getModules(keyword);
--- server/routes/shopping.router.js
+++ server/routes/shopping.router.js
@@ -7,20 +7,31 @@ const router = express.Router();
 /******************************************/
 /*              GET REQUESTS              */
 /******************************************/
-
+router.get('/:id', (req, res) => {
+    console.log('in the get route', req.params.id);
+    let queryString = 'SELECT id FROM shopping_list WHERE id = $1';
+    pool.query(queryString, [req.params.id])
+        .then(result => {
+            res.send(result);
+        })
+        .catch(err => {
+            console.log('hit error on getting object', err);
+            res.sendStatus(500);
+        });
+})//end get
 
 
 /******************************************/
 /*             POST REQUESTS              */
 /******************************************/
 router.post('/', (req, res) => {
-    let queryString = 'INSERT INTO shopping_list (name) VALUES ($1)';
-    console.log(req.body.name);
-    pool.query(queryString, [req.body.name])
+    let queryString = 'INSERT INTO shopping_list (name, date, user_created_by) VALUES ($1, $2, $3) RETURNING id';
+    console.log('result log', req.body);
+    pool.query(queryString, [req.body.name, req.body.date, req.body.user_created_by])
         .then(result => {
-            console.log('query results', result);
-            res.sendStatus(201);
-        })
+            // console.log('query results', result);
+            res.send(result);
+        })//end then 
         .catch(err => {
             console.log('hit error on posting of new Item', err);
             res.sendStatus(500);
