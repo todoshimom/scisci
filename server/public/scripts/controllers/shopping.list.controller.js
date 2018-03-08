47c43ff98ee0e30d26702b04a6193a4cfe798d48
Monica Wheeler <monica.c.wheeler@gmail.com>
2018-03-08 14:04:51 -0600

    Looping through list

--- server/public/scripts/controllers/shopping.list.controller.js
+++ server/public/scripts/controllers/shopping.list.controller.js
@@ -2,6 +2,8 @@ myApp.controller('ShoppingListController', ['ShoppingListService', function (Sho
     console.log('ShoppingListController created');
     let self = this;
 
+    self.shoppingLists = ShoppingListService.shoppingLists;
+
     self.addShoppingList = function(list) {
         ShoppingListService.addShoppingList(list);
     }//function to add selected shopping list to table
