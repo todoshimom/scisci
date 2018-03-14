de4186a8a3fcbbc7d19b7a4c11720f75851a88f5
Darian N <darian.nasrabadi@gmail.com>
2018-03-14 12:33:09 -0500

    Correct computations for costs and labor rates now being sent from server and now displaying on the reports page

--- server/models/module.costs.js
+++ server/models/module.costs.js
@@ -3,16 +3,16 @@ const pool = require('../modules/pool.js');
 // const laborRate = require('./labor.rate')
 
 // function test() {
-        
+
 //     console.log('new function');
-    
+
 //     laborRate()//getting labor rates.
 //         .then((results) => {
 //             let data = results[0].labor_rate
 //             console.log('This is the data 55', data);
-            
+
 //             return data
-            
+
 //         })
 //         .catch((error) => {
 //             console.log('Error on retrieving module costs function', error);
@@ -23,62 +23,83 @@ let getCosts = function (moduleId) {
 
     // let labRate = test()
     // console.log(labRate, 'LAB RATES');
-    
+
     let queryText
 
     if (moduleId) {
         console.log('Getting one');
         queryText = `
-        SELECT components_modules.component_id, components_modules.module_id, components.pieces_per_unit,
-        components.price_per_unit, components_modules.pieces_per_kit, appsettings.labor_rate, modules.estimated_assembly_time 
+        SELECT components_modules.component_id, components_modules.module_id, components.pieces_per_unit, components.price_per_unit, modules.name, sum(CASE WHEN modules_shopping.quantity>0 THEN modules_shopping.quantity ELSE 0 END) AS module_quantity, components_modules.pieces_per_kit, appsettings.labor_rate, modules.estimated_assembly_time 
         FROM appsettings, components
         JOIN components_modules ON components.id = components_modules.component_id
         JOIN modules ON components_modules.module_id = modules.id
+        LEFT OUTER JOIN modules_shopping on modules.id = modules_shopping.module_id
+        GROUP BY components_modules.component_id, components_modules.module_id, components.pieces_per_unit, components.price_per_unit, modules.name, components_modules.pieces_per_kit, appsettings.labor_rate, modules.estimated_assembly_time
         WHERE components_modules.module_id = ${moduleId};`;
     }
     else {
         console.log('Getting All');
 
         queryText = `
-        SELECT components_modules.component_id, components_modules.module_id, components.pieces_per_unit,
-        components.price_per_unit, components_modules.pieces_per_kit, appsettings.labor_rate, modules.estimated_assembly_time 
+        SELECT components_modules.component_id, components_modules.module_id, components.pieces_per_unit, components.price_per_unit, modules.name, sum(CASE WHEN modules_shopping.quantity>0 THEN modules_shopping.quantity ELSE 0 END) AS module_quantity, components_modules.pieces_per_kit, appsettings.labor_rate, modules.estimated_assembly_time 
         FROM appsettings, components
         JOIN components_modules ON components.id = components_modules.component_id
-        JOIN modules ON components_modules.module_id = modules.id;`;
+        JOIN modules ON components_modules.module_id = modules.id
+        LEFT OUTER JOIN modules_shopping on modules.id = modules_shopping.module_id
+        GROUP BY components_modules.component_id, components_modules.module_id, components.pieces_per_unit, components.price_per_unit, modules.name, components_modules.pieces_per_kit, appsettings.labor_rate, modules.estimated_assembly_time
+        ORDER BY modules.name;`;
     }
 
     return pool.query(queryText)
         .then((results) => {
             let data = results.rows
+
+
+            let moduleArray = [];
+            let currentSum = 0;
+            let currentKitSum = 0;
+            let module_quantity = 0;
+            let laborCost = 0;
+            let currentModule;
+
             for (let i = 0; i < data.length; i++) {
                 //If the units to order and the kit are an exact number
                 if (data[i].pieces_per_unit == data[i].pieces_per_kit) {
                     data[i].material_cost = Number(data[i].price_per_unit);
                     data[i].material_kit_cost = Number(data[i].price_per_unit);
-                    data[i].labor_cost = data[i].labor_rate * data[i].estimated_assembly_time;
-                    data[i].material_labor_cost = data[i].material_cost + data[i].labor_cost;
-                    data[i].material_kit_labor_cost = data[i].material_kit_cost + data[i].labor_cost;
                 }// If the pieces to order is more than what is required in the kit.
                 else if (data[i].pieces_per_unit > data[i].pieces_per_kit) {
                     data[i].material_cost = Number(data[i].price_per_unit);
                     let percent = data[i].pieces_per_kit / data[i].pieces_per_unit;
-                    data[i].material_kit_cost = data[i].pieces_per_kit * (percent * data[i].price_per_unit);
-                    data[i].labor_cost = data[i].labor_rate * data[i].estimated_assembly_time;
-                    data[i].material_labor_cost = data[i].material_cost + data[i].labor_cost;
-                    data[i].material_kit_labor_cost = data[i].material_kit_cost + data[i].labor_cost;
+                    data[i].material_kit_cost = percent * data[i].price_per_unit;
                 }
                 else {// If the pieces to order is less than what is required in the kit.
                     let divide = data[i].pieces_per_kit / data[i].pieces_per_unit;
                     data[i].material_cost = Math.ceil(divide) * data[i].price_per_unit;
                     data[i].material_kit_cost = data[i].price_per_unit * divide;
-                    data[i].labor_cost = data[i].labor_rate * data[i].estimated_assembly_time;
-                    data[i].material_labor_cost = data[i].material_cost + data[i].labor_cost;
-                    data[i].material_kit_labor_cost = data[i].material_kit_cost + data[i].labor_cost;
                 }
 
+                data[i].labor_cost = data[i].labor_rate * data[i].estimated_assembly_time;
+
+                // inside a group o fmodules
+                if (currentModule === data[i].module_id) {
+                    moduleArray[moduleArray.length - 1].labordata.push(data[i]);
+                    // sum stuff
+                    moduleArray[moduleArray.length - 1].currentSum += data[i].material_cost;
+                    moduleArray[moduleArray.length - 1].currentKitSum += data[i].material_kit_cost;
+                } else { // new module
+                    moduleArray.push({ moduleid: data[i].module_id, currentSum: 0, currentKitSum: 0, labordata: [] });
+                    currentModule = data[i].module_id;
+                    moduleArray[moduleArray.length - 1].labordata.push(data[i]);
+                    // sum 
+                    moduleArray[moduleArray.length - 1].currentSum += data[i].material_cost;
+                    // sum kit
+                    moduleArray[moduleArray.length - 1].currentKitSum += data[i].material_kit_cost;
+                    moduleArray[moduleArray.length - 1].laborCost = data[i].labor_rate * data[i].estimated_assembly_time;
+                    moduleArray[moduleArray.length - 1].module_quantity = data[i].module_quantity;
+                }
             }
-            // console.log(data[1], 'logging sample data results');
-            return data
+            return moduleArray
         })
         .catch((error) => {
             console.log('Error for cost calculations request', error);
--- server/routes/report.router.js
+++ server/routes/report.router.js
@@ -12,8 +12,35 @@ const moduleCost = require('../models/module.costs')
 
 router.get('/modules', authenticated, isAdmin, (req, res) => { //Start of get module reports function    
     moduleCost()//Getting all results.
-        .then((results) => {
-            res.send(results)
+        .then((laborCosts) => {
+            // const queryText = `
+            // SELECT modules.name, modules.id, sum(modules_shopping.quantity)
+            // FROM modules
+            // JOIN modules_shopping ON modules.id = modules_shopping.module_id
+            // GROUP BY modules.name, modules.id`;
+            // pool.query(queryText)
+            //     .then((results) => {
+            //         for (let i = 0; i < results.rows.length; i++) {
+            //             for (let j = 0; j < laborCosts.length; j++) {
+            //                 if (laborCosts[j].module_id === results.rows[i].id) {
+            //                     laborCosts[j].module_name = results.rows[i].name;
+            //                     laborCosts[j].module_times_used = results.rows[i].sum;
+            //                     laborCosts[j].total_mikalc = laborCosts[j].material_kit_labor_cost * results.rows[i].sum
+            //                 }
+            //             }
+            //         }
+            //         res.send(laborCosts);
+            //     })
+            //     .catch((error) => {
+            //         console.log('Error on GET modules request', error);
+            //         res.sendStatus(500);
+            //     });
+            for (let i = 0; i < laborCosts.length; i++) {
+                laborCosts[i].totalKitSum = laborCosts[i].currentKitSum * laborCosts[i].module_quantity
+                laborCosts[i].currentKitSum = laborCosts[i].currentKitSum + laborCosts[i].laborCost
+                laborCosts[i].currentSum = laborCosts[i].currentSum + laborCosts[i].laborCost
+            }
+            res.send(laborCosts);
         })
         .catch((error) => {
             console.log('Error on retrieving module costs function', error);
