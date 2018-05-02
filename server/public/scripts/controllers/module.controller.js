e2bc47581af16fa4235e872175fe7b5db67043d0
Andrew Johnson <ajj@byu.net>
2018-05-02 16:53:41 -0500

    Fix auto-save for module creator

--- server/public/scripts/controllers/module.controller.js
+++ server/public/scripts/controllers/module.controller.js
@@ -1,4 +1,4 @@
-myApp.controller('ModuleController', ['ModuleService', 'ComponentService', '$http', '$routeParams', '$scope', '$rootScope', '$timeout', function (ModuleService, ComponentService, $http, $routeParams, $scope, $rootScope, $timeout) {
+myApp.controller('ModuleController', ['ModuleService', 'ComponentService', '$http', '$routeParams', '$scope', '$rootScope', '$timeout', '$interval', function (ModuleService, ComponentService, $http, $routeParams, $scope, $rootScope, $timeout, $interval) {
     console.log('ModuleController created');
     let self = this;
 
@@ -41,8 +41,41 @@ myApp.controller('ModuleController', ['ModuleService', 'ComponentService', '$htt
     self.isSavedModule = {value: false};
     if($routeParams.id) {
         self.isSavedModule.value = true;
+
+        // Auto-save: check four times a second for changes, and auto-save them.
+        $interval(function() {
+
+            if (self.hasUnsavedChanges.status) {
+                
+                // track the number of required forms that are invalid
+                let requiredUnfilledCount = 0;
+
+                // get all the inputs that are required (they have class 'requiredForSubmission')
+                let requiredInputs = document.querySelectorAll('.requiredForSubmission');
+
+                // check to see if any of them is empty
+                for (let i = 0; i < requiredInputs.length; i++) {
+                    // if one is empty
+                    if (requiredInputs[i].value === '') {
+                        // increase the count
+                        requiredUnfilledCount++;
+                    }
+                }
+                
+                // if they're all valid, save it and reset the unsaved marker
+                if (requiredUnfilledCount > 0) {
+                    self.hasUnsavableChanges.status = true;
+                } else {
+                    self.hasUnsavableChanges.status = false;
+                    self.saveModule();
+                }
+            }
+        }, 250);
     };
 
     self.showAllComponents = false;
+
+
+
 
 }]);
